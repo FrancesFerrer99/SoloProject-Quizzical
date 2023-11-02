77090422e7c20ef5452e4b2a8bf5439853454ec3
Frances <frances.ferrer1999@gmail.com>
2023-11-02 11:52:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from 'react'
+import Question from './Components/Question'
+
+function App() {
+  const [questionData, setQuestionData] = useState([])
+  const [questionsArray, setQuestionsArray] = useState([])
+  const [userAnswersArray, setUserAnswersArray] = useState(new Array(5))
+  const [loading, setLoading] = useState(true)
+  const [gameState, setGameState] = useState(0)
+  const [correctAnswers, setCorrectAnswers] = useState(0)
+
+  useEffect(()=>{
+    dataFetch()
+  },[])
+
+  useEffect(()=>{
+    if(questionData?.length > 0){
+      setQuestionsArray(getQuestionsArray())
+      setLoading(false)
+    }
+  }, [questionData])
+
+  async function dataFetch(){
+    try{
+      const rss = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      const data = await rss.json()
+      setQuestionData(data.results)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  function getQuestionsArray(){
+      const questions = questionData.map(({ incorrect_answers, correct_answer, question })=>{
+        const {array, randIndex} = shuffle(incorrect_answers, correct_answer)
+        return {
+          question: question,
+          answers: array,
+          answerIndex: randIndex,
+        }
+      })
+      return questions
+  }
+
+  function shuffle(array, answer){
+    const randIndex = Math.floor(Math.random() * array.length)
+    array.splice(randIndex, 0, answer)
+    return{array, randIndex}
+  }
+  
+  function checkAnswers(){
+    userAnswersArray.forEach((answer, index)=>{
+      console.log(answer, questionsArray[index].answerIndex)
+      if(answer == questionsArray[index].answerIndex)
+        setCorrectAnswers(prev => prev+1)
+    })
+    setGameState(1)
+  }
+
+  function setNewGame(){
+    setCorrectAnswers(0)
+    setGameState(0)
+    setLoading(true)
+    dataFetch()
+  }
+
+  function updateAnswersArray(index, answer){
+    const newArray = [...userAnswersArray]
+    newArray[index] = answer
+    setUserAnswersArray(newArray)
+  }
+
+  const quizzForm = questionsArray.map( ({question, answers, answerIndex}, index) => {
+    return (
+        <Question
+          key={index}
+          question={question}
+          answers={answers}
+          answerIndex={answerIndex}
+          gameState={gameState}
+          updateAnswersArray={updateAnswersArray}
+          position={index}
+        />
+    )
+  })
+
+  return (
+    <>
+      {
+        !loading ? 
+          <div className='container'>
+            {quizzForm}
+            <div>
+              {
+                gameState ?
+                  <>
+                    <p>You got {correctAnswers} </p>
+                    <button onClick={setNewGame} className='submit-button'>Load new quizz</button>
+                  </> :
+                      <button onClick={checkAnswers} className='submit-button'>Check answers</button>
+              }
+            </div>
+          </div>
+        :
+        <h1>Loading...</h1>
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Question.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import { decode } from "html-entities"
+
+export default function Question({question, answers, answerIndex, gameState, updateAnswersArray, position}){
+    const [selected, setSelected] = useState()
+
+    useEffect(() => {
+        setSelected(null)
+    }, [question])
+
+    const questionText = decode(question)
+    const disable = gameState ? true : false
+    
+    const options = answers.map((option, index) => {
+        const text = decode(option)
+        const classOption = `option ${!gameState ? selected == index ? 'selected' : ''
+                                        : index == answerIndex ? 'correct' 
+                                        : selected != answerIndex && index == selected ? 'wrong' : ''}`
+
+        return(
+            <button onClick={()=> {
+                setSelected(index)
+                updateAnswersArray(position, index)
+            }} key={index} className={`${classOption}`} disabled={disable}>
+                {text}
+            </button>
+        )
+    })
+
+    return(
+        <>
+            <h2>{questionText}</h2>
+            <section>
+                {options}
+            </section>
+            <hr/>
+        </>
+    )
+}
